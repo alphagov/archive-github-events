4c3175d330a8cbda44cc3dbed2680c4ef09f2f8f
David King <david.king@digital.cabinet-office.gov.uk>
2017-05-23 15:39:41 +0100

    Validate the MAC provided by GitHub
    Only archive events we have validated as originating from Github. The SHA1 HMAC
    attached to each request as a header uses a shared secret which we pass in using
    an environment variable.
    
    We first implemented this using KMS to encrypt the shared secret but that
    doesn't seem necessary for now.

--- archive_event.py
+++ archive_event.py
@@ -1,7 +1,10 @@
 import os
 import json
-import boto3
+import hmac
+import hashlib
 import logging
+
+import boto3
 
 from datetime import datetime
 
@@ -9,7 +12,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 s3 = boto3.resource('s3')
-bucket_name = os.getenv('S3_BUCKET_NAME')
+BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+GITHUB_SECRET = os.getenv('GITHUB_SECRET').encode('utf8')
 
 def s3_key_name(event):
     headers = event['headers']
@@ -21,15 +25,26 @@ def s3_key_name(event):
         event_id=headers['X-GitHub-Delivery'],
     )
 
-
+def validate_signature(body, signature):
+    github_hmac = hmac.new(GITHUB_SECRET, msg=body, digestmod=hashlib.sha1)
+    hex_digest = "sha1={}".format(github_hmac.hexdigest())
+    return len(hex_digest) == len(signature) and hmac.compare_digest(hex_digest, signature)
 
 def lambda_handler(event, context):
-    logger.info("Event: " + str(event))
-    logger.info("Context: " + str(context))
-
-    s3.Object(bucket_name, s3_key_name(event)).put(Body=event['body'])
-    return {
-        'statusCode': 200,
-        'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps({ 'status': 'OK'})
-    }
+    body = event['body']
+    github_signature = event['headers']['X-Hub-Signature']
+    if validate_signature(body.encode('utf8'), github_signature):
+        logger.info("Successfully validated MAC")
+        s3.Object(BUCKET_NAME, s3_key_name(event)).put(Body=body)
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'status': 'OK'})
+        }
+    else:
+        logger.warn("Invalid MAC")
+        return {
+            'statusCode': 403,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'status': 'Forbidden'})
+        }
